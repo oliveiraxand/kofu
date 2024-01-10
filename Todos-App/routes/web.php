82e30d16ca943d987f2c66e7c72828b53d21074f
oliveiraxand <alexandre.osantos253@gmail.com>
2024-01-10 10:49:08 -0300

    refactor: create a prefix for todo

--- Todos-App/routes/web.php
+++ Todos-App/routes/web.php
@@ -22,16 +22,18 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('todos/index', [TodoController::class , 'index'])->name('todos.index');
-
-Route::get('todos/create', [TodoController::class, 'create'])->name('todos.create');
-
-Route::post('todos/store', [TodoController::class, 'store'])->name('todos.store');
-
-Route::get('todos/show/{id}', [TodoController::class, 'show'])->name('todos.show');
-
-Route::get('todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
-
-Route::put('todos/update', [TodoController::class, 'update'])->name('todos.update');
-
-Route::delete('todos/destroy', [TodoController::class, 'destroy'])->name('todos.destroy');
+Route::prefix('todos')->as('todos.')->controller(TodoController::class)->group(function() {
+    Route::get('index', 'index')->name('index');
+    
+    Route::get('create', 'create')->name('create');
+    
+    Route::post('store', 'store')->name('store');
+    
+    Route::get('show/{id}', 'show')->name('show');
+    
+    Route::get('{id}/edit', 'edit')->name('edit');
+    
+    Route::put('update', 'update')->name('update');
+    
+    Route::delete('destroy', 'destroy')->name('destroy');
+});
